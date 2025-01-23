b48f45ff7e65a16a7c1ed8d2825b2ee68f19d991
Jdcisneros <jorge_08.8@hotmail.com>
2025-01-23 10:23:41 -0300

    deploy 2

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,20 +3,14 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-// Verificar que DATABASE_URL esté definida
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL no está definida en las variables de entorno");
-}
-
-// Usar DATABASE_URL para la conexión
-const sequelize = new Sequelize(process.env.DATABASE_URL!, {
+const sequelize = new Sequelize(process.env.SUPABASE_URL!, {
   dialect: 'postgres',
   dialectOptions: {
     ssl: {
-      rejectUnauthorized: false, // Requerido para algunas bases de datos en la nube, como Railway
+      rejectUnauthorized: false,
     },
   },
-  logging: false, // Opcional: si no quieres ver las consultas SQL
+  logging: false,
 });
 
 export { sequelize };
