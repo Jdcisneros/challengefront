cf2027d2ff19d4204dcba5119ad7573d93eaa3f6
Jdcisneros <jorge_08.8@hotmail.com>
2025-01-22 23:42:55 -0300

    arrelo2

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,13 +3,18 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+// Verificar que DATABASE_URL esté definida
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL no está definida en las variables de entorno");
+}
+
 // Usar DATABASE_URL para la conexión
 const sequelize = new Sequelize(process.env.DATABASE_URL!, {
-  dialect: 'postgres', // O 'mysql' si es el caso
+  dialect: 'postgres',
   dialectOptions: {
     ssl: {
-      rejectUnauthorized: false // Esto es necesario para algunas bases de datos en la nube
-    }
+      rejectUnauthorized: false, // Requerido para algunas bases de datos en la nube, como Railway
+    },
   },
   logging: false, // Opcional: si no quieres ver las consultas SQL
 });
